a34b343c37827246db6cf7fd795dbae1772b9b30
Alex Jeffery <apjeffery136@googlemail.com>
2014-02-27 22:36:22 +0000

    Removed unicode characters from variable names in Interpreter for better editor/font compatability

--- Interpreter.hs
+++ Interpreter.hs
@@ -8,33 +8,33 @@ import qualified Data.Map as M
 type Store = M.Map String Expression
 
 evalProg :: Expression -> Program -> Expression
-evalProg input (Program rd comm wrt) = evalExprNorm σ' wrt
-    where σ' = evalComm (M.singleton rd input) comm
+evalProg input (Program rd comm wrt) = evalExprNorm str' wrt
+    where str' = evalComm (M.singleton rd input) comm
 
 evalComm :: Store -> Command -> Store
-evalComm σ (Compos a b) = evalComm σ' b
-    where σ' = evalComm σ a
-evalComm σ (Assign v x) = M.insert v (evalExprNorm σ x) σ
-evalComm σ (While  x c) = case evalExprNorm σ x of
-    Nil -> σ
-    _   -> evalComm σ' (While x c)
-        where σ' = evalComm σ c
+evalComm str (Compos a b) = evalComm str' b
+    where str' = evalComm str a
+evalComm str (Assign v x) = M.insert v (evalExprNorm str x) str
+evalComm str (While  x c) = case evalExprNorm str x of
+    Nil -> str
+    _   -> evalComm str' (While x c)
+        where str' = evalComm str c
 
 evalExpr :: Store -> Expression -> Expression
-evalExpr σ expr = case expr of
-    Var s         -> case M.lookup s σ of { Nothing -> Nil ; Just x -> x }
+evalExpr str expr = case expr of
+    Var s         -> case M.lookup s str of { Nothing -> Nil ; Just x -> x }
     Hd (Cons a b) -> a
     Hd Nil        -> Nil
-    Hd other      -> Hd (evalExpr σ other)
+    Hd other      -> Hd (evalExpr str other)
     Tl (Cons a b) -> b
     Tl Nil        -> Nil
-    Tl other      -> Tl (evalExpr σ other)
-    IsEq a b | evalExprNorm σ a == evalExprNorm σ b -> Cons Nil Nil
-             | otherwise                            -> Nil
-    Cons a b      -> Cons (evalExpr σ a) (evalExpr σ b)
+    Tl other      -> Tl (evalExpr str other)
+    IsEq a b | evalExprNorm str a == evalExprNorm str b -> Cons Nil Nil
+             | otherwise                                -> Nil
+    Cons a b      -> Cons (evalExpr str a) (evalExpr str b)
     Nil           -> Nil
 
 evalExprNorm :: Store -> Expression -> Expression
-evalExprNorm σ exp | nextReduce == exp = exp
-                   | otherwise       = evalExprNorm σ nextReduce
-    where nextReduce = evalExpr σ exp
+evalExprNorm str exp | nextReduce == exp = exp
+                     | otherwise         = evalExprNorm str nextReduce
+    where nextReduce = evalExpr str exp
