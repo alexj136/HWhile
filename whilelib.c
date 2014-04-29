fa24a4016600b3667e5701dc0915837613795d6b
Alex Jeffery <apjeffery136@googlemail.com>
2014-04-29 17:34:25 +0100

    Continued compiler to C, added a .gitignore

---
+++ whilelib.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <malloc.h>
+#include <assert.h>
+#include "whilelib.h"
+
+/*
+ * Allocate on the heap using malloc and assert that it was successful.
+ */
+void *ckMalloc(int size) {
+    void *ptr = malloc(size);
+    assert(ptr);
+    return ptr;
+}
+
+/*
+ * Create a new nil node on the heap, return a pointer to it.
+ */
+Node *newNil() {
+    Node *nilNode = ckMalloc(sizeof(Node));
+    nilNode->nodeType = nil;
+    return nilNode;
+}
+
+/*
+ * Build a cons node from a left and right subtree. The given trees are part of
+ * the returned trees - they are not copied, so freeing a tree given as an
+ * argument to this function will cause crazy errors. Don't do it.
+ */
+Node *build(Node *left, Node *right) {
+    Node *consNode = ckMalloc(sizeof(Node));
+    consNode->nodeType = cons;
+    consNode->left = left;
+    consNode->right = right;
+    return consNode;
+}
+
+/*
+ * Free an entire tree.
+ */
+void freeTree(Node *root) {
+    if(root->nodeType == cons) {
+        freeTree(root->left);
+        freeTree(root->right);
+    }
+    else {
+        assert(root->nodeType == nil);
+    }
+    free(root);
+    return;
+}
+
+/*
+ * Make a copy of the given tree on the heap.
+ */
+Node *copyTree(Node *root) {
+    if(root->nodeType == nil) {
+        return newNil();
+    }
+    else {
+        return build(copyTree(root->left), copyTree(root->right));
+    }
+}
+
+/*
+ * Build an entirely new tree that is the cons of the two given trees. The given
+ * subtrees can be safely freed without affecting the returned tree.
+ */
+Node *takeCons(Node *left, Node *right) {
+    return build(copyTree(left), copyTree(right));
+}
+
+/*
+ * Build an entirely new tree that is the head of the given tree. The given tree
+ * can be safely freed without affecting the returned tree.
+ */
+Node *takeHead(Node *root) {
+    return copyTree(root->left);
+}
+
+/*
+ * Build an entirely new tree that is the tail of the given tree. The given tree
+ * can be safely freed without affecting the returned tree.
+ */
+Node *takeTail(Node *root) {
+    return copyTree(root->right);
+}
+
+/*
+ * Determine whether or not two trees have the exact same structure.
+ */
+bool treeEqual(Node* a, Node *b) {
+    if(a->nodeType == nil && b->nodeType == nil) {
+        return true;
+    }
+    else {
+        return treeEqual(a->left, b->left) && treeEqual(a->right, b->right);
+    }
+}
---
+++ whilelib.h
@@ -0,0 +1,36 @@
+/*
+ * Header file for whilelib.c.
+ * Contains data definitions for data types used by compiled while programs.
+ */
+
+#ifndef WHILELIB
+#define WHILELIB
+
+typedef enum {
+    nil,
+    cons
+} NodeType;
+
+typedef enum {
+    false,
+    true
+} bool;
+
+typedef struct Node Node;
+struct Node {
+    NodeType nodeType;
+    Node *left;
+    Node *right;
+};
+
+void *ckMalloc(int size);
+Node *newNil();
+Node *build(Node *left, Node *right);
+void freeTree(Node *root);
+Node *copyTree(Node *root);
+Node *takeCons(Node *left, Node *right);
+Node *takeHead(Node *root);
+Node *takeTail(Node *root);
+bool treeEqual(Node* a, Node *b);
+
+#endif
