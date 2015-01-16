ae330ef6692e60a93ffe0ab132470e65ba1a5997
Alex Jeffery <apjeffery136@googlemail.com>
2015-01-16 16:59:29 +0000

    Work on Sugar.hs

--- Sugar.hs
+++ Sugar.hs
@@ -8,9 +8,29 @@ module Sugar where
 import Syntax
 
 data SuCommand
-    = Compos SuCommand SuCommand
-    | Assign String Expression
-    | While  Expression SuCommand
-    | IfElse Expression SuCommand SuCommand
-    | PMatch Expression [(Expression, SuCommand)]
+    = SuCompos SuCommand SuCommand
+    | SuAssign String Expression
+    | SuWhile  Expression SuCommand
+    | SuIfElse Expression SuCommand SuCommand
+    | SuPMatch Expression [(Expression, SuCommand)]
     deriving Eq
+
+-- Desugar a command, that is, convert it to pure while syntax
+desugar :: SuCommand -> Command
+desugar suComm = case suComm of
+    SuCompos c1 c2    -> Compos (desugar c1) (desugar c2)
+    SuAssign x  exp   -> Assign x exp
+    SuWhile  gd c     -> While gd (desugar c)
+    SuIfElse gd c1 c2 ->
+        Compos (Compos (Compos (Compos (Compos
+            (Assign "+NOT+EXP+STACK+" (Cons (Cons Nil Nil) (Var "+NOT+EXP+STACK+")))
+            (Assign "+EXP+VAL+STACK+" (Cons gd (Var "+EXP+VAL+STACK+"))))
+            (While (Hd (Var "+EXP+VAL+STACK+")) (Compos (Compos
+                (Assign "+EXP+VAL+STACK+" (Cons Nil (Tl (Var "+EXP+VAL+STACK+"))))
+                (Assign "+NOT+EXP+STACK+" (Cons Nil (Tl (Var "+NOT+EXP+STACK+")))))
+                c1)))
+            (While (Hd (Var "+NOT+EXP+STACK+")) (Compos
+                (Assign "+NOT+EXP+STACK+" (Cons Nil (Tl (Var "+NOT+EXP+STACK+"))))
+                c2)))
+            (Assign "+NOT+EXP+STACK+" (Tl (Var "+NOT+EXP+STACK+"))))
+            (Assign "+EXP+VAL+STACK+" (Tl (Var "+EXP+VAL+STACK+")))
